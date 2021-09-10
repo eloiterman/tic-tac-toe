c71dcda9d5eba1ebbbf1299a5246d7aca5c4c309
Evan Loiterman <evanloiterman@gmail.com>
2021-09-10 16:48:42 -0500

    Added Controlled X State

--- src/index.js
+++ src/index.js
@@ -19,39 +19,50 @@ reportWebVitals();
 
  
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
- 
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 
+
  
 
 
 
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
 
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i}/>;
-    }
-  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
   render() {
     const status = 'Next player: X';
- 
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -74,6 +85,7 @@ class Square extends React.Component {
     );
   }
 }
+
  
 class Game extends React.Component {
   render() {
